e16759dcd28e94b6fbe9b110bb1385dfcf3515a8
Saugat <ghimiresaugat987@gmail.com>
2024-04-19 20:41:26 -0400

    feat: Game, Menu, Board and Tetris Components

---
+++ src/components/Tetris.js
@@ -0,0 +1,11 @@
+import "./Tetris.css";
+import Board from "./Board";
+const Tetris = ({ rows, columns, setGameOver }) => {
+
+
+    return (
+        <Board />
+    );
+};
+
+export default Tetris;
